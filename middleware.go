c8dd8cddb2928c7e526c42267b041a4bc6794b80
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2024-01-06 17:34:55 +0200

    authenticating handlers

--- middleware.go
+++ middleware.go
@@ -1,7 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/nosurf"
+)
 
 func (a *application) LoadSession(next http.Handler) http.Handler {
 	return a.session.LoadAndSave(next)
+}
+
+func (a *application) authRequired(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userId := a.session.GetInt(r.Context(), sessionKeyUserId)
+		if userId == 0 {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+
+		w.Header().Add("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	}
+}
+
+func (a *application) CSRFTokenRequired(next http.Handler) http.Handler {
+	handler := nosurf.New(next)
+
+	return handler
 }
